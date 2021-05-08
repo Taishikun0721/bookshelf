fdf1f714bc7ad90acd4944996650cb4208a802d3
Taishikun0721 <tai0721abc@gmail.com>
2021-05-08 21:15:49 +0900

    book一覧画面の作成

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -1,7 +1,15 @@
 require 'features_helper'
 
 RSpec.describe 'display each books on the page' do
+  let(:repository) { BookRepository.new }
+  before do
+    repository.clear
+
+    repository.create(title: 'PoEAA', author: 'Martin Fowler')
+    repository.create(title: 'TDD',   author: 'Kent Beck')
+  end
   it 'is displays each book on the page' do
+
     visit '/books'
 
     within '#books' do
